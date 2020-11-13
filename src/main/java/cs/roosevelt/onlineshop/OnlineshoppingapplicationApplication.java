00039095fe11e785b32b27f5204eee24d452dca7
louisdiaz <lemunoz8@gmail.com>
2020-11-13 12:03:07 -0600

    Initial project upload.

---
+++ src/main/java/cs/roosevelt/onlineshop/OnlineshoppingapplicationApplication.java
@@ -0,0 +1,13 @@
+package cs.roosevelt.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshoppingapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs/roosevelt/onlineshop/OnlineshoppingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.roosevelt.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshoppingapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
